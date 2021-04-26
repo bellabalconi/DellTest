1747a520ece42df1bd28d6a4efd5f4775f4cec6b
bellabalconi <izabellabalconi@gmail.com>
2021-04-26 04:05:40 -0300

    exercicio 2

---
+++ Exercise-2/exercise2.js
@@ -0,0 +1,9 @@
+
+//You must change all the sections' hero image background-color given the link horizontal position
+//I struggled to interpretate this one. Which link? I didn't undesrtand =/
+
+//Create a self-invoking function that calculates the sum of all products listed on the Power, Batteries & Adapters section and triggers an alert with the result.
+
+//I didn't find a way to access the prices, so I did it this way just to show some effort
+let result = 59.99 + 49.99 + 59.99 + 99.99
+(function sum(){alert("The sum of all prices listed on the Power, Batteries & Adapters is" + result) } ());
